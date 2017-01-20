c8a54c832dca6b5a2284f9bbf64b2640cdc7fcb0
philip <philip.clarke@redhood.co.uk>
2017-01-20 17:31:06 +0000

    refactored the string conversion into a function

---
+++ tests/test-helper.js
@@ -0,0 +1,6 @@
+var jsdom = require('jsdom').jsdom;
+
+global.document = jsdom('<html><head><script></script></head><body></body></html>');
+global.window = global.document.parentWindow;
+global.navigator = window.navigator = {};
+global.Node = window.Node;
