33ad2b2a282f2920fab700247f543e5180f6ec68
philip <philip.clarke@redhood.co.uk>
2017-01-20 12:22:05 +0000

    made the code a bit more tidy

--- public/core.js
+++ public/core.js
@@ -8,22 +8,16 @@ app.controller('MainCtrl', function($scope, $http, $timeout){
     var arrayone = $scope.wordone.toLowerCase().split("").sort();
     var arraytwo = $scope.wordtwo.toLowerCase().split("").sort();
 
-
-
-       if (arrayone.toString() === arraytwo.toString()){
-           $scope.message=$scope.wordone + " is an anagram of " + $scope.wordtwo;
-         } else {
-        $scope.message=$scope.wordone + " is not an anagram of " + $scope.wordtwo;
+    if (arrayone.toString() === arraytwo.toString()){
+         $scope.message=$scope.wordone + " is an anagram of " + $scope.wordtwo;
+        } else {
+          $scope.message=$scope.wordone + " is not an anagram of " + $scope.wordtwo;
        }
 
+    $timeout(function () { $scope.message = ""; }, 3000);
 
-       $timeout(function () { $scope.message = ""; }, 3000);
+    $scope.wordone='';
+    $scope.wordtwo='';
 
-
-          $scope.wordone='';
-          $scope.wordtwo='';
-
-
-
-        };
+    };
 });
